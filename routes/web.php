333f2483a71ae5c45c55d9c96381949dd49c0e48
guaracyalima <guaracyaraujolima@gmail.com>
2018-04-10 14:17:54 -0300

    to heroku

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,14 @@
 
 Route::get('/', function () {
     //return view('welcome');
-    $query = http_build_query([
-       'client_id' => '3',
-       'redirect_uri' => 'http://localhost:9999/callback',
-        'response_type' =>  'code',
-        'scope' => ''
-    ]);
-
-    return redirect("http://127.0.0.1:8000/oauth/authorize?$query");
+//    $query = http_build_query([
+//       'client_id' => '3',
+//       'redirect_uri' => 'http://localhost:9999/callback',
+//        'response_type' =>  'code',
+//        'scope' => ''
+//    ]);
+//
+//    return redirect("http://127.0.0.1:8000/oauth/authorize?$query");
 });
 
 Auth::routes();
