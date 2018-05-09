1fb78e4d9984e0d8ab6050182f8e6347ac759e25
guaracyalima <guaracyaraujolima@gmail.com>
2018-05-09 16:23:06 -0300

    add signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,4 +35,16 @@ class AuthController extends Controller
         return User::with(['client', 'notification', 'alerts'])->where('cpf', '=', $cpf)->get();
     }
 
+    public function signup(Request $request)
+    {
+        $data = $request->all();
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+            'cpf' => $data['cpf'],
+            'role' => 'client'
+        ]);
+    }
+
 }
