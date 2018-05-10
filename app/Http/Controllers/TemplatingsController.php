a464386bd4e7ec7d37849ecbd15b10d18e135a34
guaracyalima <guaracyaraujolima@gmail.com>
2018-05-10 12:40:59 -0300

    url de retorno de imagens geradas como sendo https

--- app/Http/Controllers/TemplatingsController.php
+++ app/Http/Controllers/TemplatingsController.php
@@ -246,11 +246,11 @@ class TemplatingsController extends Controller
         $imageName = md5('brasalimg') . '.jpg';
         $img->save(public_path('img/' . $imageName));
 
-        $data['media_name'] = "http://api-bseg.brasal.com.br/{$imageName}";
+        $data['media_name'] = "https://api-bseg.brasal.com.br/img/{$imageName}";
         $data['user_id'] = 11;
 
         return [
-            'path' => 'http://api-bseg.brasal.com.br/' . $imageName
+            'path' => 'https://api-bseg.brasal.com.br/img/' . $imageName
         ];
 
     }
