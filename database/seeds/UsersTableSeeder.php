2dd0758e334685510f639ce08e4c8cc8401c7da9
guaracyalima <guaracyaraujolima@gmail.com>
2018-05-15 19:13:42 -0300

    users seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,47 +22,47 @@ class UsersTableSeeder extends Seeder
             'remember_token' => str_random(10)]);
 
 
-//        factory(\App\Entities\User::class)->create([
-//            'name' => 'News Clients Department',
-//            'email' => 'newsclient@bseg.com',
-//            'password' => bcrypt(123456),
-//            'role' => 'news',
-//            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
-//            'remember_token' => str_random(10)]);
-//
-//
-//        factory(\App\Entities\User::class)->create([
-//            'name' => 'Healt Departament',
-//            'email' => 'healt@bseg.com',
-//            'password' => bcrypt(123456),
-//            'role' => 'healt',
-//            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
-//            'remember_token' => str_random(10)]);
-//
-//
-//        factory(\App\Entities\User::class)->create([
-//            'name' => 'Eterprise Departament',
-//            'email' => 'enterprise@bseg.com',
-//            'password' => bcrypt(123456),
-//            'role' => 'enterprise',
-//            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
-//            'remember_token' => str_random(10)]);
-//
-//
-//        factory(\App\Entities\User::class)->create([
-//            'name' => 'Renovation Center',
-//            'email' => 'renovationcenter@bseg.com',
-//            'password' => bcrypt(123456),
-//            'role' => 'cr',
-//            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
-//            'remember_token' => str_random(10)]);
-//
-//        factory(\App\Entities\User::class)->create([
-//            'name' => 'Collaborator',
-//            'email' => 'collaborator@bseg.com',
-//            'password' => bcrypt(123456),
-//            'role' => 'collaborator',
-//            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
-//            'remember_token' => str_random(10)]);
+        factory(\App\Entities\User::class)->create([
+            'name' => 'News Clients Department',
+            'email' => 'newsclient@bseg.com',
+            'password' => bcrypt(123456),
+            'role' => 'news',
+            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
+            'remember_token' => str_random(10)]);
+
+
+        factory(\App\Entities\User::class)->create([
+            'name' => 'Healt Departament',
+            'email' => 'healt@bseg.com',
+            'password' => bcrypt(123456),
+            'role' => 'healt',
+            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
+            'remember_token' => str_random(10)]);
+
+
+        factory(\App\Entities\User::class)->create([
+            'name' => 'Eterprise Departament',
+            'email' => 'enterprise@bseg.com',
+            'password' => bcrypt(123456),
+            'role' => 'enterprise',
+            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
+            'remember_token' => str_random(10)]);
+
+
+        factory(\App\Entities\User::class)->create([
+            'name' => 'Renovation Center',
+            'email' => 'renovationcenter@bseg.com',
+            'password' => bcrypt(123456),
+            'role' => 'cr',
+            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
+            'remember_token' => str_random(10)]);
+
+        factory(\App\Entities\User::class)->create([
+            'name' => 'Collaborator',
+            'email' => 'collaborator@bseg.com',
+            'password' => bcrypt(123456),
+            'role' => 'collaborator',
+            'cpf' => rand(1, 999) . rand(1, 999) . rand(1, 999) . rand(1, 99),
+            'remember_token' => str_random(10)]);
     }
 }
